42e8917a613e1b0fa97f4dab5842e00ea26ed0c4
tooru-lgtm <84662088+tooru-lgtm@users.noreply.github.com>
2021-09-05 10:42:08 +0900

    Add files via upload

---
+++ controllers/sign-out.php
@@ -0,0 +1,17 @@
+<?php
+///////////////////////////////////////
+// サインアウトコントローラー
+///////////////////////////////////////
+ 
+// 設定を読み込む
+include_once 'C:\xampp\htdocs\TwitterClone\config.php';
+// 便利な関数を読み込む
+include_once 'C:\xampp\htdocs\TwitterClone\util.php';
+ 
+// ユーザー情報をセッションから削除
+deleteUserSession();
+ 
+// ログイン画面に遷移
+header('Location: ' . HOME_URL . '\controllers\sign-in.php');
+exit;
+
