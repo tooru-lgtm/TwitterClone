b42fb6b89e49a449022aa8dd454605eebc192fe8
tooru-lgtm <84662088+tooru-lgtm@users.noreply.github.com>
2021-09-05 17:55:32 +0900

    Add files via upload

---
+++ controllers/sign-out.php
@@ -0,0 +1,17 @@
+<?php
+///////////////////////////////////////
+// サインアウトコントローラー
+///////////////////////////////////////
+ 
+// 設定を読み込む
+include_once '../config.php';
+// 便利な関数を読み込む
+include_once '../util.php';
+ 
+// ユーザー情報をセッションから削除
+deleteUserSession();
+ 
+// ログイン画面に遷移
+header('Location: ' . HOME_URL . '\controllers\sign-in.php');
+exit;
+
